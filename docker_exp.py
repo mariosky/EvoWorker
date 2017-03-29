26cc4d8b48efd4e847d7933d9e081c9b628bed43
mariosky <mariosky@gmail.com>
2017-03-29 07:15:37 -0700

    REDIS_HOST env

--- docker_exp.py
+++ docker_exp.py
@@ -8,9 +8,9 @@ import json
 
 
 #Set IP of REDIS box. Redis Host is sent to workers so it can not be 127.0.0.1
-REDIS_HOST= 'REDIS_HOST' in os.environ and int(os.environ['REDIS_HOST']) or '192.168.1.13'
+REDIS_HOST= 'REDIS_HOST' in os.environ and os.environ['REDIS_HOST'] or '192.168.1.13'
 #Experiment ID must be an integer
-EXPERIMENT_ID = 1003
+EXPERIMENT_ID = 904
 
 DATA_ROOT = './experiment_data'
 DATA_FOLDER = './experiment_data/' + str(EXPERIMENT_ID)
@@ -26,7 +26,7 @@ es_conf = {
 }
 
 
-for function in (1,2):
+for function in (4):
 
     for dim in (2, 3, 5, 10, 20, 40):
         print "DIM", dim
